e3e94c630a7ac2d6de3a1ab599c88aacb8f668ff
AmmarYasser2017 <ammar.yasser20173@gmail.com>
2025-01-31 22:56:08 +0200

    create project structure

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kMainColor = Color(0xff22C7B8);
+const kLightGrey = Color(0xff707070);
+const kDarkGrey = Color(0xff404040);
+const kBlue = Color(0xff5F9EDC);
+const kGreen = Color(0xff5FDC89);
+const kPurple = Color(0xff915FDC);
+const kBrown = Color(0xffDC915F);
